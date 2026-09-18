725a3a499e15bdb02b22389a15d2db5decacf761
Manuel Ayala <136390439+manuayalag@users.noreply.github.com>
2026-09-18 17:15:37 -0500

    Revert "Feature/box builder"

--- src/features/product-experience/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export { RIBBON_COLORS, type RibbonColorOption } from "./ribbon-colors";
-export {
-  CARD_MESSAGE_MAX_LENGTH,
-  PACKAGING_OPTIONS,
-  type PackagingOption,
-} from "./packaging-options";
